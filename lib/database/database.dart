6b7212072744c890b53347c154973fcd315a32f4
rodrigo kakiuchi de souza <rodrigo.kakiuchi2010@gmail.com>
2025-06-03 02:03:54 -0300

    Add User model and update database to user model

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_prova2/models/user.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
@@ -11,48 +12,55 @@ class DatabaseHelper {
 
   DatabaseHelper.internal();
 
-  Future<Database?> get database async {
-    if (internal != null) return internal;
-    internal = await initDatabase();
-    return internal;
-  }
-
   Future<Database> initDatabase() async {
     return openDatabase(
       join(await getDatabasesPath(), 'user_database.db'),
       onCreate: (db, version) {
         return db.execute(
-          "CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, password TEXT)",
+          "CREATE TABLE user(id INTEGER PRIMARY KEY AUTOINCREMENT, name Text, email TEXT, password TEXT)",
         );
       },
       version: 1,
     );
   }
 
-  Future<void> insertUser(String email, String password) async {
+  Future<Database?> get database async {
+    if (internal != null) return internal;
+    internal = await initDatabase();
+    return internal;
+  }
+
+
+  Future<void> insertUser(User user) async {
     final db = await database;
+
     await db!.insert(
-      'users',
-      {'email': email, 'password': password},
+      'user',
+      user.toMap(),
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
   }
 
-  Future<Map<String, dynamic>?> getUser(String email) async {
+  Future<User?> getUserByEmail(String email) async {
     final db = await database;
-    final List<Map<String, dynamic>> users = await db!.query(
-      'users',
+
+    final List<Map<String, dynamic>> result = await db!.query(
+      'user',
       where: "email = ?",
       whereArgs: [email],
     );
-    if (users.isNotEmpty) {
-      return users.first;
+
+    if (result.isNotEmpty) {
+      return User.fromMap(result.first);
     }
     return null;
   }
 
-  Future<List<Map<String, dynamic>>> getAllUsers() async {
-    final db = await database;
-    return await db!.query('users');
-  }
+  // Future<List<User>> getAllUsers() async {
+  //   final db = await database;
+  //   final List<Map<String, dynamic>> result = await db!.query('user');
+  //   return List.generate(result.length, (i) {
+  //     return User.fromMap(result[i]);
+  //   });
+  // }
 }
